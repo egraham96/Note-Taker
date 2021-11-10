e3e4a5a178d9da5a4d955637c3b76f13bdd4b9a2
Emma <eeg4@uw.edu>
2021-11-10 00:20:38 -0800

    Final Final

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3020;
+const port = process.env.PORT || 3001
 
 const app = express();
 
@@ -29,6 +29,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port} 🚀`)
 );
